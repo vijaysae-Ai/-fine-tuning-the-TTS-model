7d5b0718822a3e7c76897792669b3fdee1f0a2a0
vijaysae-Ai <vijay.sae20@gmail.com>
2023-09-04 15:58:27 +0530

    requirements and setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name='Ims-Toucan',
+    version=1.0,
+    author='vijayakumar',
+    author_email='vijay.sae20@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/vijaysae-Ai/Ims-toucan/tree/main',
+    license='MIT',
+    install_requires=['numpy','pandas'])
